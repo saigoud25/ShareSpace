66cbf69d15de8d3e9239e549736b653fe51de58c
sai kumar <saimamindlagoud@gmail.com>
2022-07-25 15:19:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,6 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 const CONNECTION_URL = 'mongodb+srv://sai:1234@cluster0.irpa7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-// const CONNECTION_URL = 'mongodb+srv://js_mastery:M6WfDnJEoj9HkV2d@practice.jto9p.mongodb.net/memories_app?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
